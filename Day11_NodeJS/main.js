e29751ccc8be2dca86af2186f6e6caa284ff96ce
Likhilesh <likbalpande@example.com>
2024-06-21 15:29:51 +0530

    day11-all

--- Day11_NodeJS/main.js
+++ Day11_NodeJS/main.js
@@ -3,23 +3,28 @@ const fsPromises = require("fs/promises");
 const fs = require("fs");
 const url = require("url");
 
-const dataText = fs.readFileSync("./data.json");
+const dataText = fs.readFileSync(`${__dirname}/data.json`);
 const data = JSON.parse(dataText);
 
 const app = http.createServer(async (req, res) => {
     res.writeHead(200, {
         "Content-Type": "text/html",
     });
+
     const { query, pathname } = url.parse(req.url, true);
 
     switch (pathname) {
         case "/": {
-            const bf = await fsPromises.readFile("./pages/homepage.html");
+            const bf = await fsPromises.readFile(
+                `${__dirname}/pages/homepage.html`
+            );
             res.end(bf);
             break;
         }
         case "/products": {
-            const bf = await fsPromises.readFile("./pages/productsPage.html");
+            const bf = await fsPromises.readFile(
+                `${__dirname}/pages/productsPage.html`
+            );
             let text = bf.toString();
             let productsText = "";
             for (let i = 0; i < data.length; i++) {
@@ -35,7 +40,24 @@ const app = http.createServer(async (req, res) => {
             break;
         }
         case "/view": {
-            res.end(`<div><h2>This is product id = ${query.id}</h2></div>`);
+            const product = data.find((elem) => {
+                if (elem.id == query.id) return true;
+                else return false;
+            });
+            const bf = await fsPromises.readFile(
+                `${__dirname}/pages/view.html`
+            );
+            let text = bf.toString();
+            text = text.replace(
+                "$VIEW$",
+                `<div>
+                    <h2>${product.title}</h2>
+                    <img src="${product.thumbnail}" height='300'>
+                    <h4>${product.price}</h4>
+                    <p>${product.description}</p>
+                </div>`
+            );
+            res.end(text);
             break;
         }
         default: {
