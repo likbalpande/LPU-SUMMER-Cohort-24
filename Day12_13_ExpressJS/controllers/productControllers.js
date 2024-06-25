1d59f4209db518ae4f07c60df2efd3d390a7cd40
Likhilesh <likbalpande@example.com>
2024-06-26 01:37:25 +0530

    middlewares - validateID, getData

--- Day12_13_ExpressJS/controllers/productControllers.js
+++ Day12_13_ExpressJS/controllers/productControllers.js
@@ -12,6 +12,38 @@ const validateForTitleAndPrice = (req, res, next) => {
     next();
 };
 
+const getDataMiddleware = async (req, res, next) => {
+    const text = await fsPromises.readFile("./data.json", { encoding: "utf8" });
+    let products;
+    try {
+        products = JSON.parse(text);
+    } catch {
+        products = [];
+    }
+    req.products = products;
+    next();
+};
+
+const validateID = (req, res, next) => {
+    const params = req.params;
+    const { products } = req;
+
+    const prIdx = products.findIndex((elem) => {
+        if (elem.id == params.id) return true;
+        return false;
+    });
+    if (prIdx === -1) {
+        res.status(400);
+        res.json({
+            status: "fail",
+            message: "!! Invalid product id",
+        });
+        return;
+    }
+    req.productId = prIdx;
+    next();
+};
+
 const getData = async () => {
     const text = await fsPromises.readFile("./data.json", { encoding: "utf8" });
     let products;
@@ -24,7 +56,7 @@ const getData = async () => {
 };
 
 const getProducts = async (req, res) => {
-    let products = await getData();
+    let products = req.products;
     res.json({
         status: "success",
         data: {
@@ -35,7 +67,7 @@ const getProducts = async (req, res) => {
 
 const createProduct = async (req, res) => {
     const body = req.body;
-    const products = await getData();
+    const products = req.products;
     let lastId = 0;
     if (products.length != 0) {
         lastId = products[products.length - 1].id;
@@ -54,21 +86,9 @@ const createProduct = async (req, res) => {
 };
 
 const replaceProduct = async (req, res) => {
-    const params = req.params;
     const body = req.body;
-    const products = await getData();
-    const prIdx = products.findIndex((elem) => {
-        if (elem.id == params.id) return true;
-        return false;
-    });
-    if (prIdx === -1) {
-        res.status(400);
-        res.json({
-            status: "fail",
-            message: "Invalid product id",
-        });
-        return;
-    }
+    const products = req.products;
+    const prIdx = req.productId;
 
     body.id = products[prIdx].id;
     products[prIdx] = body;
@@ -83,20 +103,8 @@ const replaceProduct = async (req, res) => {
 };
 
 const deleteProduct = async (req, res) => {
-    const params = req.params;
-    const products = await getData();
-    const prIdx = products.findIndex((elem) => {
-        if (elem.id == params.id) return true;
-        return false;
-    });
-    if (prIdx === -1) {
-        res.status(400);
-        res.json({
-            status: "fail",
-            message: "Invalid product id",
-        });
-        return;
-    }
+    const products = req.products;
+    const prIdx = req.productId;
 
     products.splice(prIdx, 1);
     await fsPromises.writeFile("./data.json", JSON.stringify(products));
@@ -111,21 +119,9 @@ const deleteProduct = async (req, res) => {
 };
 
 const updateProduct = async (req, res) => {
-    const params = req.params;
     const body = req.body;
-    const products = await getData();
-    const prIdx = products.findIndex((elem) => {
-        if (elem.id == params.id) return true;
-        return false;
-    });
-    if (prIdx === -1) {
-        res.status(400);
-        res.json({
-            status: "fail",
-            message: "Invalid product id",
-        });
-        return;
-    }
+    const products = req.products;
+    const prIdx = req.productId;
 
     const newOBJ = {
         ...products[prIdx],
@@ -150,4 +146,6 @@ module.exports = {
     deleteProduct,
     updateProduct,
     validateForTitleAndPrice,
+    getDataMiddleware,
+    validateID,
 };
--- Day12_13_ExpressJS/routes/productRoutes.js
+++ Day12_13_ExpressJS/routes/productRoutes.js
@@ -6,12 +6,20 @@ const {
     deleteProduct,
     updateProduct,
     validateForTitleAndPrice,
+    getDataMiddleware,
+    validateID,
 } = require("../controllers/productControllers.js");
 
 const productRouter = express.Router();
 
+productRouter.use(getDataMiddleware);
+
 productRouter.route("/").get(getProducts).post(validateForTitleAndPrice, createProduct);
 
-productRouter.route("/:id").put(validateForTitleAndPrice, replaceProduct).patch(updateProduct).delete(deleteProduct);
+productRouter
+    .route("/:id")
+    .put(validateID, validateForTitleAndPrice, replaceProduct)
+    .patch(validateID, updateProduct)
+    .delete(validateID, deleteProduct);
 
 module.exports = productRouter;
