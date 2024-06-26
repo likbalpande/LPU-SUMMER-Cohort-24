20a882d6ae0e0dea55e96eb8c810907ccb173316
Likhilesh <likbalpande@example.com>
2024-06-26 16:59:20 +0530

    reduced connection code

--- Day14/database/db.js
+++ Day14/database/db.js
@@ -7,18 +7,10 @@ dbURL = dbURL.replace("<username>", process.env.DB_USERNAME);
 dbURL = dbURL.replace("<password>", process.env.DB_PASSWORD);
 dbURL = dbURL.replace("<dbName>", process.env.DB_NAME);
 
-const client = new MongoClient(dbURL, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-
+const client = new MongoClient(dbURL);
 const database = client.db(process.env.DB_NAME);
 const productsCollection = database.collection("products");
 
 module.exports = {
-    database,
     productsCollection,
 };
