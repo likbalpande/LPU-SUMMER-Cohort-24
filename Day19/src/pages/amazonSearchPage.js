088d54561f0482e7db221328571ecc4d23f35a3d
Likhilesh <likbalpande@example.com>
2024-07-04 14:52:20 +0530

    useState: get data

--- Day19/src/pages/amazonSearchPage.js
+++ Day19/src/pages/amazonSearchPage.js
@@ -1,30 +1,34 @@
-import CategoryBar from "../components/categoryBar";
-import Navbar from "../components/navbar";
+import { useState } from "react";
 
 const SearchPage = (props) => {
-    const { categories } = props;
-
-    const customStyles = {
-        padding: "48px",
-        textAlign: "center",
-        backgroundColor: "Yellow",
-    };
-
-    let searchText = "";
+    // let searchText = "";
+    const [searchText, setSearchText] = useState("");
     console.log("initially: ", searchText);
 
     const handleSearch = (e) => {
         const val = e.target.value;
-        console.log(val);
+        // searchText = val;
+        setSearchText(val);
     };
+
+    const [products, setProducts] = useState([]);
+    async function getData() {
+        const res = await fetch("https://dummyjson.com/products");
+        const data = await res.json();
+        setProducts(data.products);
+    }
 
     return (
         <div>
-            <Navbar />
-            <CategoryBar categories={categories} />
-            <div style={customStyles}>
+            <div>
                 <input type="text" onChange={handleSearch} />
             </div>
+            <h1>The search Text is : {searchText}</h1>
+            <hr />
+            <button onClick={getData}>Get Data</button>
+            {products.map((elem) => {
+                return <p>{elem.title}</p>;
+            })}
         </div>
     );
 };
