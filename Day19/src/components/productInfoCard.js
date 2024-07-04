1f59802fc6f54e814c8ab79646d8e16c288de7db
Likhilesh <likbalpande@example.com>
2024-07-04 11:06:32 +0530

    Day20: p1: Amazon homepage upto some extent

--- Day19/src/components/productInfoCard.js
+++ Day19/src/components/productInfoCard.js
@@ -1,5 +1,18 @@
-const ProductInfoCard = (obj) => {
-    return <div className="products-info-card">{obj.title}</div>;
+const ProductInfoCard = (props) => {
+    const { data = {} } = props;
+    return (
+        <div className="products-info-card">
+            <h3>{data.title}</h3>
+            <div className="products-item-cards-container">
+                {data.products.map((elem) => (
+                    <div className="products-item-card">
+                        <img src={elem.img} />
+                        <h5>{elem.title}</h5>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default ProductInfoCard;
--- Day19/src/pages/homePage.js
+++ Day19/src/pages/homePage.js
@@ -20,8 +20,86 @@ const productInfoCards = [
         title: "Revamp",
         products: [
             {
-                title: "Cushion",
-                img: "https://www.abc.com",
+                title: "Air Conditioners",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-372x232----B08RDL6H79._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Refrigerators",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B08345R1ZW---372x232._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Microwaves",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B07G5J5FYP._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Washing Machines",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08CPQVLZT._SY232_CB667322346_.jpg",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Revamp",
+        products: [
+            {
+                title: "Air Conditioners",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-372x232----B08RDL6H79._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Refrigerators",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B08345R1ZW---372x232._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Microwaves",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B07G5J5FYP._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Washing Machines",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08CPQVLZT._SY232_CB667322346_.jpg",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Revamp",
+        products: [
+            {
+                title: "Air Conditioners",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-372x232----B08RDL6H79._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Refrigerators",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B08345R1ZW---372x232._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Microwaves",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B07G5J5FYP._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Washing Machines",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08CPQVLZT._SY232_CB667322346_.jpg",
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "Revamp",
+        products: [
+            {
+                title: "Air Conditioners",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-372x232----B08RDL6H79._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Refrigerators",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B08345R1ZW---372x232._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Microwaves",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/B07G5J5FYP._SY232_CB667322346_.jpg",
+            },
+            {
+                title: "Washing Machines",
+                img: "https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08CPQVLZT._SY232_CB667322346_.jpg",
             },
         ],
     },
@@ -65,7 +143,7 @@ const HomePage = () => {
                 />
                 <div className="products-cards-container">
                     {productInfoCards.map((elem) => {
-                        return <ProductInfoCard title={elem.title} />;
+                        return <ProductInfoCard data={elem} />;
                     })}
                 </div>
             </div>
