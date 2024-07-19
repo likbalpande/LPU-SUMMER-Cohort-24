8b7c2a5536467468c8b389d6f431c06fafcbd2a9
Likhilesh <likbalpande@example.com>
2024-07-19 14:49:55 +0530

    frontend of OTP page: ready

--- CloudHome/frontend/app.js
+++ CloudHome/frontend/app.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import appStore from "./src/store/appStore";
 import AppRouter from "./appRouter";
+import "./globalStyles.css";
 
 const App = () => {
     return (
         <Provider store={appStore}>
-            <AppRouter />;
+            <AppRouter />
         </Provider>
     );
 };
--- CloudHome/frontend/src/pages/otpPage.js
+++ CloudHome/frontend/src/pages/otpPage.js
@@ -1,18 +1,39 @@
 import { useState } from "react";
+import Navbar from "../components/navbar";
+import { useSelector } from "react-redux";
 
 const OtpPage = () => {
+    const { email } = useSelector((e) => e.auth);
     const [otp, setOtp] = useState();
 
     const handleSubmit = () => {
-        console.log("otp is", otp);
+        if (otp.length < 4) {
+            alert("Invalid OTP");
+        } else {
+            const num = parseInt(otp);
+            if (num >= 1000 && num <= 9999) {
+                alert(num);
+            } else {
+                alert("Invalid OTP. OTP must be Number");
+            }
+        }
     };
 
     return (
-        <div className="otp-page-container">
-            <p>Email :</p>
-            <input type="number" value={otp} onChange={(e) => setOtp(e.target.value)} />
-            <button onClick={handleSubmit}>Verify</button>
-        </div>
+        <>
+            <Navbar />
+            <div className="otp-page-container">
+                <p>Email : {email}</p>
+                <div className="otp-input-container">
+                    <input maxLength={4} type="text" value={otp} onChange={(e) => setOtp(e.target.value)} />
+                    <div className="otp-column c1" />
+                    <div className="otp-column c2" />
+                    <div className="otp-column c3" />
+                    <div className="otp-column c4" />
+                </div>
+                <button onClick={handleSubmit}>Verify</button>
+            </div>
+        </>
     );
 };
 
