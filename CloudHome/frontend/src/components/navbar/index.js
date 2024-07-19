c1d6acaffb2b5611871725dfbb43e0bc99ab881a
Likhilesh <likbalpande@example.com>
2024-07-19 13:16:58 +0530

    logout

--- CloudHome/frontend/src/components/navbar/index.js
+++ CloudHome/frontend/src/components/navbar/index.js
@@ -1,13 +1,21 @@
 import "./styles.css";
+import { useDispatch } from "react-redux";
+import { appLogout } from "../../store/slices/authSlice";
 
 const Navbar = () => {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(appLogout());
+    };
+
     return (
         <div className="navbar-container">
             <div className="navbar-left-items">
                 <h3>Cloud Home</h3>
             </div>
             <div className="navbar-right-items">
-                <button>Logout</button>
+                <button onClick={handleLogout}>Logout</button>
             </div>
         </div>
     );
